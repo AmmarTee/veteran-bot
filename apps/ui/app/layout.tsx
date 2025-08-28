9ffadda45e6c77f1453e526245d4d0bea8ebabd5
AmmarTee <ammarthedev@gmail.com>
2025-08-29 02:11:31 +0500

    Removed Let's encrypt and Trafiek

--- apps/ui/app/layout.tsx
+++ apps/ui/app/layout.tsx
@@ -9,10 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body>
         {children}
       </body>
     </html>
   )
 }
-
