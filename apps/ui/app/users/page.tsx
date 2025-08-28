e511350495a6758beaa43823a4347f34846bde98
AmmarTee <ammarthedev@gmail.com>
2025-08-29 03:35:40 +0500

    Fixed some errors, Had Coffee

--- apps/ui/app/users/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function UsersPage() {
-  return (
-    <div>
-      <h1 className="text-xl font-semibold">Users</h1>
-      <p className="text-gray-600">Coming soon.</p>
-    </div>
-  )
-}
-
